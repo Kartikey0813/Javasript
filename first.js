3ebf177bdceae97378c000127dc959dbf2eededf
Kartikey Mishra <kartikeymishra@Kartikey-MacBook-Air.local>
2024-07-11 21:13:46 +0530

    First files

---
+++ first.js
@@ -0,0 +1,32 @@
+console.log("Kartikey Mishra")
+
+const student = {
+    name : "Kartikey Mishra",
+    age : 20,
+    cgpa : 10,
+    isPass : true
+}; 
+
+console.log(student["isPass"]);
+
+console.log("Practise");
+
+const product = {
+    name : "Parker Jotter Standard CT Ball Pen",
+    rating : 4.8,
+    offer : "40% off",
+    mrp : 270,
+};
+
+const profile = {
+    name : "kartikeyMishra",
+    post : 24,
+    followers : 1014,
+    following : 410,
+    intro : "Student at IIT-M",
+    isFollow : true
+};
+
+
+
+
